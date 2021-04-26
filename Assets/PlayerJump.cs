3eeb9efee755adb70fd1f3401283fc93c027bda5
Lynxire <funedy@yandex.ru>
2021-04-26 20:27:58 +0300

    first commit

---
+++ Assets/PlayerJump.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerJump : MonoBehaviour
+{
+
+    [SerializeField] private float _speedWild;
+    [SerializeField] private Transform _target;
+    [SerializeField] private float _speedTargetMove;
+    
+    private void Update()
+    {
+        //GetComponent<Rigidbody>().velocity = Vector3.left * _speedWild;
+        if(Input.GetMouseButtonDown(0))
+        {
+            Vector3 _targetDir;
+            if (_target.transform.position.x > transform.position.x)
+                _targetDir = Vector3.right;
+            else
+                _targetDir = Vector3.left;
+
+            GetComponent<Rigidbody>().AddForce(Vector3.up * _speedWild + _targetDir * _speedTargetMove);
+
+        }
+    }
+
+}
---
+++ Assets/Portal.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal : MonoBehaviour
+{
+
+    [SerializeField] private Transform _transform;
+    public bool _isPlayerState;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<PlayerJump>()) 
+        {
+            if (!_isPlayerState)
+            {
+                _transform.GetComponent<Portal>()._isPlayerState = true;
+                other.transform.position = new Vector3(_transform.position.x, other.transform.position.y, other.transform.position.z);
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.GetComponent<PlayerJump>())
+        {
+            _isPlayerState = false;
+        }
+    }
+
+}
